8f2ea8e3db681aad5cd7638be551278d96ed473e
alan <alan@debian.com>
2023-11-08 23:59:36 -0600

    Primer commit

---
+++ frontend/src/components/Announcement.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Announcement = () => {
+  return <Container>Obten un 10% de descuento con el cupo TEW</Container>;
+};
+
+export default Announcement;
---
+++ frontend/src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,52 @@
+import "./featuredInfo.css";
+import { useEffect, useState } from "react";
+import { userRequest } from "../../admin/src/requestMethods";
+
+export default function FeaturedInfo() {
+  const [income, setIncome] = useState([]);
+  const [perc, setPerc] = useState(0);
+
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await userRequest.get("orders/income");
+        setIncome(res.data);
+        setPerc((res.data[1].total * 100) / res.data[0].total - 100);
+      } catch {}
+    };
+    getIncome();
+  }, []);
+
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <span className="featuredTitle">Revanue</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">${income[1]?.total}</span>
+          <span className="featuredMoneyRate">
+            %{Math.floor(perc)}{" "}
+            </span>
+        </div>
+        <span className="featuredSub">Compared to last month</span>
+      </div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Sales</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$4,415</span>
+          <span className="featuredMoneyRate">
+          </span>
+        </div>
+        <span className="featuredSub">Compared to last month</span>
+      </div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Cost</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$2,225</span>
+          <span className="featuredMoneyRate">
+          </span>
+        </div>
+        <span className="featuredSub">Compared to last month</span>
+      </div>
+    </div>
+  );
+}
